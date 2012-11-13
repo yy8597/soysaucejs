a8a23b8102d6b5dbd1a3b5b90974217026bc33a7
Andrew Hoffman <andrew.hoffman@brandingbrand.com>
2012-11-13 11:29:21 -0500

    Revert "Including the actual files wouldn't hurt..."
    This reverts commit 7ddda95f5713cce125c7ff51a9279161254957fd.

--- scripts.js
+++
@@ -1,7 +0,0 @@
-$(document).ready(function() {
-  document.getElementById("myModal").classList.toggle('on');
-  $(".modal .close-window").on("click", function(e){
-   e.preventDefault();
-   $(this).closest('.modal').removeClass('on');
-  });
-});
