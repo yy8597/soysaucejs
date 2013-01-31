fa55a3013cbaf014eb462c960c6c3b94b713faa9
Edward Gaba <egaba88@gmail.com>
2013-01-31 11:43:09 -0500

    cc validation - begin 2 stage cc validation

--- assets/javascript/soysauce/widgets/cc_validation.js
+++ assets/javascript/soysauce/widgets/cc_validation.js
@@ -1,39 +1,72 @@
-soysauce.ccValidation = (function() {
-	$("body").on("keyup", "[data-ss-widget='cc_validation']", function(e) {
-		var card_num = e.target.value.replace(/-/g, "");
+soysauce.ccValidators = (function() {
+	var validators = new Array();
+	
+	function ccValidator(input) {
+		var self = this;
 		
-		// Stage 1
-		if(card_num.length === 1 || $(".cardContainer .fade").length === 0) {
-			if(card_num.match(/^4/))
-				$(".cardContainer span:not(.visa)").addClass("fade");
-			else if(card_num.match(/^5/))
-				$(".cardContainer span:not(.mc)").addClass("fade");
-			else if(card_num.match(/^3/))
-				$(".cardContainer span:not(.amex,.dc)").addClass("fade");
-			else if(card_num.match(/^6(?:011|5[0-9]{2})/))
-				$(".cardContainer span:not(.disc)").addClass("fade");
-			else if(card_num.match(/^(?:2131|1800|35\d{3})/))
-				$(".cardContainer span:not(.jcb)").addClass("fade");
-			else
-				$(".cardContainer span").addClass("fade");
-		}
-		else if(card_num.length === 0)
-			$(".cardContainer span").removeClass("fade");
+		this.id = parseInt($(input).attr("data-ss-id"));
+		this.input = $(input);
+		this.state1;
+		this.state2;
 		
-		// Stage 2
-		if(card_num.match(/^4[0-9]{12}(?:[0-9]{3})?$/))
-			$(e.target).attr("ss-state", "visa");
-		else if(card_num.match(/^5[1-5][0-9]{14}$/))
-			$(e.target).attr("ss-state", "mastercard")
-		else if(card_num.match(/^3[47][0-9]{13}$/))
-			$(e.target).attr("ss-state", "amex")
-		else if(card_num.match(/^3(?:0[0-5]|[68][0-9])[0-9]{11}$/))
-			$(e.target).attr("ss-state", "dinersclub")
-		else if(card_num.match(/^6(?:011|5[0-9]{2})[0-9]{12}$/))
-			$(e.target).attr("ss-state", "discover")
-		else if(card_num.match(/^(?:2131|1800|35\d{3})\d{11}$/))
-			$(e.target).attr("ss-state", "jcb")
-		else
-			$(e.target).attr("ss-state", "undefined");
-	});
+		this.input.on("keyup change", function(e) {
+			var card_num = e.target.value.replace(/-/g, "");
+			
+			// State 1
+			if (card_num.length === 1) {
+				$(e.target).trigger("state1");
+				if (card_num.match(/^4/)) {
+					self.state1 = "visa";
+				} else if (card_num.match(/^5/)) {
+					self.state1 = "mastercard";
+				} else if (card_num.match(/^3/)) {
+					self.state1 = "amex dinersclub";
+				} else if (card_num.match(/^6(?:011|5[0-9]{2})/)) {
+					self.state1 = "discover";
+				} else if (card_num.match(/^(?:2131|1800|35\d{3})/)) {
+					self.state1 = "jcb";
+				} else {
+					self.state1 = undefined;
+				}
+			} else if (card_num.length === 0) {
+				self.state1 = undefined;
+			}
+
+			// State 2
+			if (card_num.match(/^4[0-9]{12}(?:[0-9]{3})?$/)) {
+				self.state2 = "visa";
+				$(e.target).trigger("state2");
+			} else if (card_num.match(/^5[1-5][0-9]{14}$/)) {
+				self.state2 = "mastercard";
+				$(e.target).trigger("state2");
+			} else if (card_num.match(/^3[47][0-9]{13}$/)) {
+				self.state2 = "amex";
+				$(e.target).trigger("state2");
+			} else if (card_num.match(/^3(?:0[0-5]|[68][0-9])[0-9]{11}$/)) {
+				self.state2 = "dinersclub";
+				$(e.target).trigger("state2");
+			} else if (card_num.match(/^6(?:011|5[0-9]{2})[0-9]{12}$/)) {
+				self.state2 = "discover";
+				$(e.target).trigger("state2");
+			} else if (card_num.match(/^(?:2131|1800|35\d{3})\d{11}$/)) {
+				self.state2 = "jcb";
+				$(e.target).trigger("state2");
+			} else {
+				self.state2 = undefined;
+			}
+				
+		});
+	}
+	
+	// Init
+	(function() {
+		$("[data-ss-widget='cc_validator']").each(function() {
+			var validator = new ccValidator(this);
+			validators.push(validator);
+		});
+		
+	})(); // end init
+	
+	
+	return validators;
 })();
