096210a128cf127a762e8be1e3b2f730dd4d09f0
Edward Gaba <egaba88@gmail.com>
2013-03-04 16:27:15 -0500

    Creating build v1.1.9

--- public/javascript/soysauce.lite.js
+++ public/javascript/soysauce.lite.js
@@ -596,34 +596,6 @@ soysauce = {
 		supportsLocalStorage: (typeof(window.localStorage) !== "undefined") ? true : false,
 		supportsSessionStorage: (typeof(window.sessionStorage) !== "undefined") ? true : false
 	},
-	freezeChildren: function(selector) {
-		var children = $("[data-ss-id='" + selector + "']").find("[data-ss-widget]");
-		children.each(function(index, child) {
-			var id = $(child).attr("data-ss-id");
-			soysauce.freeze(id, false);
-		});
-	},
-	freeze: function(selector, freezeChildren) {
-		if (typeof(selector) === "object") {
-			selector = parseInt($(selector).attr("data-ss-id"));
-		}
-		freezeChildren = (freezeChildren === undefined) ? true : false;
-		soysauce.fetch(selector).handleFreeze();
-		if (freezeChildren) {
-			soysauce.freezeChildren(selector);
-		}
-	},
-	unfreeze: function(selector) {
-		if (typeof(selector) === "object") {
-			selector = parseInt($(selector).attr("data-ss-id"));
-		}
-		var children = $("[data-ss-id='" + selector + "']").find("[data-ss-widget]");
-		soysauce.fetch(selector).handleUnfreeze();
-		children.each(function(index, child) {
-			var id = $(child).attr("data-ss-id");
-			soysauce.fetch(id).handleUnfreeze();
-		});
-	},
 	scrollTop: function() {
 		window.setTimeout(function(){
 			window.scrollTo(0, 1);
