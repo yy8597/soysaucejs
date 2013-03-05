581c81ddac1a103c5860c01aa0b1f9687d419276
Edward Gaba <egaba88@gmail.com>
2013-03-05 17:49:37 -0500

    Creating build v1.1.12

--- README.md
+++ README.md
@@ -1,14 +1,14 @@
-# Soysauce (v1.1.11)
+# Soysauce (v1.1.12)
 Original Author: Edward Gaba
 
 Soysauce is a customizable javascript widget library. Popular widgets included are carousels and accordions. Please file any issues you find in Github.
 
 ## Current CDN URLs
-* Compressed Lite (40.01 KB) - http://cdn.brandingbrand.com/soysauce/v1.1.11/soysauce.lite.min.js
-* Uncompressed Lite (61.65 KB) - http://cdn.brandingbrand.com/soysauce/v1.1.11/soysauce.lite.js
-* Compressed (44.60 KB) - http://cdn.brandingbrand.com/soysauce/v1.1.11/soysauce.min.js
-* Uncompressed (70.30 KB) - http://cdn.brandingbrand.com/soysauce/v1.1.11/soysauce.js
-* CSS (4.22 KB) - http://cdn.brandingbrand.com/soysauce/v1.1.11/soysauce.css
+* Compressed Lite (40.01 KB) - http://cdn.brandingbrand.com/soysauce/v1.1.12/soysauce.lite.min.js
+* Uncompressed Lite (61.66 KB) - http://cdn.brandingbrand.com/soysauce/v1.1.12/soysauce.lite.js
+* Compressed (44.60 KB) - http://cdn.brandingbrand.com/soysauce/v1.1.12/soysauce.min.js
+* Uncompressed (70.31 KB) - http://cdn.brandingbrand.com/soysauce/v1.1.12/soysauce.js
+* CSS (4.22 KB) - http://cdn.brandingbrand.com/soysauce/v1.1.12/soysauce.css
 
 Note: Lite contains only the bare essentials (which will later help with scaling as the number of widgets increase):
 * Fastclick
--- public/javascript/soysauce.js
+++ public/javascript/soysauce.js
@@ -1212,8 +1212,8 @@ soysauce.carousels = (function() {
 			wrapper.find("~ [data-ss-button-type='next']").attr("data-ss-state", "enabled");
 		}
 		
-		this.links = ((items[0].tagName.match(/^a$/i) !== null && items[0].tagName.match(/^a$/i) !== undefined) || items.find("a[href]").length > 0) ? true : false;
-		
+		this.links = (items[0].tagName.match(/^a$/i) !== null || items[0].tagName.match(/^a$/i) !== undefined || items.find("a[href]").length > 0) ? true : false;
+
 		if (this.thumbs) {
 			var c = 0;
 
@@ -1293,7 +1293,7 @@ soysauce.carousels = (function() {
 			var padding = parseInt(firstItem.css("padding-left")) + parseInt(firstItem.css("padding-right"));
 			var margin = parseInt(firstItem.css("margin-left")) + parseInt(firstItem.css("margin-right"));
 			
-			self.spacingOffset = padding;
+			self.spacingOffset = 0; // remove this for now
 			
 			if (self.multi) {
 				self.itemWidth = self.widget.width() / self.multiVars.numItems;
@@ -1433,7 +1433,7 @@ soysauce.carousels = (function() {
 					setTranslate(self.container[0], self.offset);
 					window.setTimeout(function() {
 						self.container.attr("data-ss-state", "intransit");
-						self.offset = -self.index*self.itemWidth + (self.peekWidth/2) + self.spacingOffset;
+						self.offset = -self.index*self.itemWidth + self.peekWidth + self.spacingOffset;
 						setTranslate(self.container[0], self.offset);
 					}, 0);
 				}, 0);
@@ -1447,7 +1447,7 @@ soysauce.carousels = (function() {
 					setTranslate(self.container[0], self.offset);
 					window.setTimeout(function() {
 						self.container.attr("data-ss-state", "intransit");
-						self.offset = -self.itemWidth + (self.peekWidth/2) + self.spacingOffset;
+						self.offset = -self.itemWidth + self.peekWidth + self.spacingOffset;
 						setTranslate(self.container[0], self.offset);
 					}, 0);
 				}, 0);
@@ -1794,7 +1794,7 @@ soysauce.carousels = (function() {
 				setTranslate(self.container[0], self.offset - dragOffset);
 			});
 		}
-		
+
 		// Decides whether to zoom or move to next/prev item
 		this.widget.one("touchend mouseup", function(e2) {
 			if (self.jumping) return;
@@ -1820,7 +1820,7 @@ soysauce.carousels = (function() {
 			var fast = (velocity > 0.9) ? true : false;
 			
 			self.widget.off("touchmove mousemove");
-
+			
 			if (!self.interrupted && self.links && Math.abs(xDist) === 0) {
 				self.ready = true;
 				self.container.attr("data-ss-state", "ready");
@@ -1831,6 +1831,7 @@ soysauce.carousels = (function() {
 				else if ($(e2.target).closest("a").length > 0) {
 					window.location.href = $(e2.target).closest("a").attr("href");
 				}
+				
 			}
 			else if (!self.interrupted && self.zoom && ((Math.abs(xDist) < 2 && Math.abs(yDist) < 2) || self.isZoomed)) {
 				soysauce.stifle(e1);
