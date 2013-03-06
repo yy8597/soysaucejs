c90b2eb01908e000988355ef2ae2176802c288c6
Edward Gaba <egaba88@gmail.com>
2013-03-06 10:43:13 -0500

    autofill zip - complete

--- assets/javascript/soysauce/widgets/autofill-zip.js
+++ assets/javascript/soysauce/widgets/autofill-zip.js
@@ -1,24 +1,45 @@
 soysauce.autofillZip = (function() {
 	
 	function autofillZip(selector) {
+		var self = this;
+		
 		this.widget = $(selector);
 		this.id = parseInt($(selector).attr("data-ss-id"));
 		this.zip = this.widget.find("> [data-ss-component='zip']");
 		this.city = this.widget.find("> [data-ss-component='city']");
 		this.state = this.widget.find("> [data-ss-component='state']");
 		
-		this.zip.on("keyup change", this.retrieveData);
+		this.zip.on("keyup change", function(e) {
+			self.retrieveData(e);
+		});
 	}
 	
 	autofillZip.prototype.retrieveData = function(e) {
 		var value = e.target.value;
-		console.log("here");
-		console.log(google);
-		if (!google) return;
+		var self = this;
+		
+		if (!soysauce.geocoder) return;
 
 		if ((value.length === 5) && (parseFloat(value) == parseInt(value)) && !isNaN(value))  {
-			console.log(value);
+			soysauce.geocoder().geocode({"address": value}, function(results, status) {
+				var city, state_long, state_short;
+				
+				if (status === "ZERO_RESULTS") return;
+				
+				city = results[0].address_components[1].long_name;
+				state_long = results[0].address_components[3].long_name;
+				state_short = results[0].address_components[3].short_name;
+				
+				self.city.val(city);
+				self.state.val(state_short);
 
+				if (self.state.val() === "") {
+					self.state.val(state_short.toLowerCase());
+					if (self.state.val() === "") {
+						self.state.val(state_long);
+					}
+				}
+			});
 		}
 	};
 	
@@ -29,78 +50,3 @@ soysauce.autofillZip = (function() {
 	};
 	
 })();
-// 
-// function is_int(value){ 
-//   if((parseFloat(value) == parseInt(value)) && !isNaN(value)){
-//     return true;
-//   } else { 
-//     return false;
-//   } 
-// }
-// 
-// // Set up
-// var firstReveal = true;
-// var skip = false;
-// 
-// // Check if URL is working
-// $.ajax({
-//   url: "http://zip.elevenbasetwo.com",
-//   cache: false,
-//   dataType: "json",
-//   type: "GET",
-// 	data: "zip=15203",
-//   success: function(result, success) {
-// 		$(".m_autofill").hide();
-//   },
-// 	error: function(result, success) {
-// 		skip = true;
-// 	}
-// });
-// 
-// if (skip) return;
-// 
-// $("#zipCode"). on("keyup change", function() {
-// 
-// 	// Cache 
-// 	var el = $(this);
-// 
-// 	// Did they type five integers?
-// 	if ((el.val().length == 5) && (is_int(el.val())))  {
-// 
-// 		// Call Ziptastic for information
-// 		$.ajax({
-// 			url: "http://zip.elevenbasetwo.com",
-// 			cache: false,
-// 			dataType: "json",
-// 			type: "GET",
-// 			data: "zip=" + el.val(),
-// 			success: function(result, success) {
-// 				setTimeout(function() {
-// 					
-// 					// Odd Cases
-// 					if (result.city === "PITT") {
-// 						result.city = "PITTSBURGH";
-// 					}
-// 					else if (result.city === "OKC") {
-// 						result.city = "Oklahoma City";
-// 					}
-// 					
-// 					$("#checkoutCity").attr("value", result.city);
-// 					$("#stateOrProvince").attr("value", result.state);
-// 					checkState($("#stateOrProvince")[0]);
-// 					
-// 					$("#city_state_zip").removeClass("field_error");
-// 				},0);
-// 
-// 				if (firstReveal) {
-// 					$(".m_autofill").show();
-// 				}
-// 				firstReveal = false;		
-// 			},
-// 			error: function() {
-// 				// Cannot find zip code
-// 				$(".m_autofill").show();
-// 			}
-// 		});
-// 	}
-// });
