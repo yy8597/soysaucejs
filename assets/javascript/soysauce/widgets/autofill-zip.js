1649d2b60ac4c37b6dd2ea32272966bcfd23f943
Edward Gaba <egaba88@gmail.com>
2013-03-07 11:55:20 -0500

    save

--- assets/javascript/soysauce/widgets/autofill-zip.js
+++ assets/javascript/soysauce/widgets/autofill-zip.js
@@ -1,4 +1,5 @@
 soysauce.autofillZip = (function() {
+	var API_KEY = "b1861a4f641d4f62a3d6483d30d25f37";
 	
 	function autofillZip(selector) {
 		var self = this;
@@ -14,11 +15,13 @@ soysauce.autofillZip = (function() {
 		});
 	}
 	
+	autofillZip.prototype.test = function() {
+		console.log("WORKING");
+	};
+	
 	autofillZip.prototype.retrieveData = function(e) {
 		var value,
 				self = this;
-		
-		if (!soysauce.geocoder) return;
 		
 		if (!e) {
 			value = this.zip[0].value;
@@ -28,27 +31,30 @@ soysauce.autofillZip = (function() {
 		}
 
 		if ((value.length === 5) && (parseFloat(value) == parseInt(value)) && !isNaN(value))  {
-			soysauce.geocoder().geocode({"address": value}, function(results, status) {
-				var city, state_long, state_short, state_index;
-				
-				if (status === "ZERO_RESULTS") return;
-				
-				city = results[0].address_components[1].long_name;
-				state_long = results[0].address_components[3].long_name;
-				
-				state_index = results[0].address_components.length - 2;
-				state_short = results[0].address_components[state_index].short_name;
-				
-				self.city.val(city);
-				self.state.val(state_short);
-
-				if (self.state.val() === "") {
-					self.state.val(state_short.toLowerCase());
-					if (self.state.val() === "") {
-						self.state.val(state_long);
-					}
-				}
-			});
+			
+			$.ajax("https://www.mapquestapi.com/geocoding/v1/address?key=Fmjtd%7Cluub2l6tnu%2Ca5%3Do5-96tw0f&location=15203&callback=soysauce.fetch(1).test");
+			
+			// soysauce.geocoder().geocode({"address": value}, function(results, status) {
+			// 				var city, state_long, state_short, state_index;
+			// 				
+			// 				if (status === "ZERO_RESULTS") return;
+			// 				
+			// 				city = results[0].address_components[1].long_name;
+			// 				state_long = results[0].address_components[3].long_name;
+			// 				
+			// 				state_index = results[0].address_components.length - 2;
+			// 				state_short = results[0].address_components[state_index].short_name;
+			// 				
+			// 				self.city.val(city);
+			// 				self.state.val(state_short);
+			// 
+			// 				if (self.state.val() === "") {
+			// 					self.state.val(state_short.toLowerCase());
+			// 					if (self.state.val() === "") {
+			// 						self.state.val(state_long);
+			// 					}
+			// 				}
+			// 			});
 		}
 	};
 	
