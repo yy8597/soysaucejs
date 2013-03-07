3722e4a39f52081e7e9e7af896dafec4979c6da3
Edward Gaba <egaba88@gmail.com>
2013-03-07 13:21:37 -0500

    autofill-zip: replace google's with mapquest due to licensing purposes (didnt wanna implement a map)

--- assets/javascript/soysauce/widgets/autofill-zip.js
+++ assets/javascript/soysauce/widgets/autofill-zip.js
@@ -1,5 +1,6 @@
 soysauce.autofillZip = (function() {
-	var API_KEY = "b1861a4f641d4f62a3d6483d30d25f37";
+	var BASE_URL = "//www.mapquestapi.com/geocoding/v1/address?key=";
+	var API_KEY = "Fmjtd%7Cluub2l6tnu%2Ca5%3Do5-96tw0f";
 	
 	function autofillZip(selector) {
 		var self = this;
@@ -9,52 +10,37 @@ soysauce.autofillZip = (function() {
 		this.zip = this.widget.find("[data-ss-component='zip']");
 		this.city = this.widget.find("[data-ss-component='city']");
 		this.state = this.widget.find("[data-ss-component='state']");
+		this.lastRequestedData;
 		
 		this.zip.on("keyup change", function(e) {
-			self.retrieveData(e);
+			self.getLocationData(e);
 		});
 	}
 	
-	autofillZip.prototype.test = function() {
-		console.log("WORKING");
-	};
-	
-	autofillZip.prototype.retrieveData = function(e) {
-		var value,
-				self = this;
+	autofillZip.prototype.setLocationData = function(data) {
+		var self = this;
 		
-		if (!e) {
-			value = this.zip[0].value;
+		this.lastRequestedData = data;
+		if (data.info.statuscode === 0) {
+			$.each(data.results[0].locations, function(i, locationData) {
+				var city = locationData.adminArea5;
+				var state = locationData.adminArea3;
+				
+				self.city.val(city);
+				self.state.val(state);
+			});
 		}
 		else {
-			value = e.target.value;
+			console.warn("Soysauce: Geocoder returned error code " + data.info.statuscode);
 		}
-
+	};
+	
+	autofillZip.prototype.getLocationData = function() {
+		var self = this;
+		var value = this.zip[0].value;
+		
 		if ((value.length === 5) && (parseFloat(value) == parseInt(value)) && !isNaN(value))  {
-			
-			$.ajax("https://www.mapquestapi.com/geocoding/v1/address?key=Fmjtd%7Cluub2l6tnu%2Ca5%3Do5-96tw0f&location=15203&callback=soysauce.fetch(1).test");
-			
-			// soysauce.geocoder().geocode({"address": value}, function(results, status) {
-			// 				var city, state_long, state_short, state_index;
-			// 				
-			// 				if (status === "ZERO_RESULTS") return;
-			// 				
-			// 				city = results[0].address_components[1].long_name;
-			// 				state_long = results[0].address_components[3].long_name;
-			// 				
-			// 				state_index = results[0].address_components.length - 2;
-			// 				state_short = results[0].address_components[state_index].short_name;
-			// 				
-			// 				self.city.val(city);
-			// 				self.state.val(state_short);
-			// 
-			// 				if (self.state.val() === "") {
-			// 					self.state.val(state_short.toLowerCase());
-			// 					if (self.state.val() === "") {
-			// 						self.state.val(state_long);
-			// 					}
-			// 				}
-			// 			});
+			$("body").append("<script src='" + BASE_URL + API_KEY + "&location=" + value + "&callback=soysauce.fetch(" + this.id + ").setLocationData" + "'></script>");
 		}
 	};
 	
