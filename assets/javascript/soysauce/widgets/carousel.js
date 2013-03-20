17ffe721f2255d3e87662db4625b98095a88fb8f
Edward Gaba <egaba88@gmail.com>
2013-03-20 12:04:06 -0400

    overlay - add checks

--- assets/javascript/soysauce/widgets/carousel.js
+++ assets/javascript/soysauce/widgets/carousel.js
@@ -948,6 +948,8 @@ soysauce.carousels = (function() {
 		// Zoom In
 		if (!this.isZoomed) {
 			var offset = 0;
+			
+			soysauce.overlay.on();
 
 			if ($(e2.target).attr("data-ss-component") === "zoom_icon") {
 				self.panCoords = {x: 0, y: 0};
