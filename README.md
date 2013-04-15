832edfdf48ef91f62231e0cc0adacfb9505f1127
Edward Gaba <egaba88@gmail.com>
2013-04-15 14:17:10 -0400

    Creating build v1.1.60

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# Soysauce (v1.1.59)
+# Soysauce (v1.1.60)
 Original Author: Edward Gaba
 
 Soysauce is a customizable javascript widget library. Popular widgets included are carousels and accordions. Please file any issues you find in Github. Please see the Soysauce website for full documentation of the widgets:
@@ -6,13 +6,13 @@ Soysauce is a customizable javascript widget library. Popular widgets included a
 http://soysauce.s3.amazonaws.com/site/home.html
 
 ## Current CDN URLs
-* Compressed (57.81 KB) - http://divgzeiu68c7e.cloudfront.net/soysauce/v1.1.59/soysauce.min.js
-* Uncompressed (88.89 KB) - http://divgzeiu68c7e.cloudfront.net/soysauce/v1.1.59/soysauce.js
-* Compressed Lite (38.37 KB) - http://divgzeiu68c7e.cloudfront.net/soysauce/v1.1.59/soysauce.lite.min.js
-* Uncompressed Lite (57.72 KB) - http://divgzeiu68c7e.cloudfront.net/soysauce/v1.1.59/soysauce.lite.js
-* Compressed Legacy (59.25 KB) - http://divgzeiu68c7e.cloudfront.net/soysauce/v1.1.59/soysauce.legacy.min.js
-* Uncompressed Legacy (92.11 KB) - http://divgzeiu68c7e.cloudfront.net/soysauce/v1.1.59/soysauce.legacy.js
-* CSS (6.95 KB) - http://divgzeiu68c7e.cloudfront.net/soysauce/v1.1.59/soysauce.css
+* Compressed (58.00 KB) - http://divgzeiu68c7e.cloudfront.net/soysauce/v1.1.60/soysauce.min.js
+* Uncompressed (89.19 KB) - http://divgzeiu68c7e.cloudfront.net/soysauce/v1.1.60/soysauce.js
+* Compressed Lite (38.34 KB) - http://divgzeiu68c7e.cloudfront.net/soysauce/v1.1.60/soysauce.lite.min.js
+* Uncompressed Lite (57.68 KB) - http://divgzeiu68c7e.cloudfront.net/soysauce/v1.1.60/soysauce.lite.js
+* Compressed Legacy (59.44 KB) - http://divgzeiu68c7e.cloudfront.net/soysauce/v1.1.60/soysauce.legacy.min.js
+* Uncompressed Legacy (92.41 KB) - http://divgzeiu68c7e.cloudfront.net/soysauce/v1.1.60/soysauce.legacy.js
+* CSS (7.15 KB) - http://divgzeiu68c7e.cloudfront.net/soysauce/v1.1.60/soysauce.css
 
 Note: Lite contains only the bare essentials (which will later help with scaling as the number of widgets increase):
 * Carousel
--- public/javascript/soysauce.legacy.js
+++ public/javascript/soysauce.legacy.js
@@ -1300,7 +1300,7 @@ soysauce.autosuggest = (function() {
 				resPos.right = resPos.left + $(self.results).width();
 
 				if (pageY < resPos.top || pageY > resPos.bottom || pageX < resPos.left || pageX > resPos.right) {
-					$(this.results).hide();
+					$(self.results).hide();
 				}
 			}).
 			focus(function(e) {
@@ -1468,7 +1468,7 @@ soysauce.autosuggest = (function() {
 	AutoSuggest.prototype.runSuggest = function(e, self) {	
 		var search = function(searchData) {
 			if (this.value.length < self.acSettings.minCharacters) {
-				clearAndHideResults();
+				self.clearAndHideResults();
 				return false;
 			}
 
@@ -2748,20 +2748,41 @@ soysauce.carousels = (function() {
 soysauce.inputClear = (function() {
 	
 	function inputClear(selector) {
-		var options = soysauce.getOptions(selector);
-		var self = this;
+		var options = soysauce.getOptions(selector),
+		    self = this,
+		    iconFocus = false;
 		
 		this.widget = $(selector);
 		this.id = parseInt($(selector).attr("data-ss-id"));
+		this.icon;
 		
 		this.widget.on("focus keyup", function() {
 			self.handleIcon();
 		});
 		
-		this.widget.wrap("<span data-ss-component='input-wrapper'></span>").attr("data-ss-clear", "off");
+		this.widget.on("blur", function() {
+			if (iconFocus) return;
+			self.widget.attr("data-ss-clear", "off");
+		});
+		
+		this.widget.wrap("<div data-ss-component='input-wrapper'></div>");
+		
+    this.widget.parent().css({
+      "display": self.widget.css("display"),
+      "width": self.widget.css("width")
+    });
+		
+		this.widget.attr("data-ss-clear", "off");
 		this.widget.after("<span data-ss-component='icon'></span>");
-		this.widget.find("+ [data-ss-component='icon']").on("click", function() {
-			self.clear();
+		
+		this.icon = this.widget.find("+ [data-ss-component='icon']");
+		
+		this.icon.on("mousedown touchstart", function() {
+			iconFocus = true;
+			self.icon.one("mouseup touchend", function() {
+				self.clear();
+				iconFocus = false;
+			});
 		});
 	}
 	
@@ -2770,16 +2791,7 @@ soysauce.inputClear = (function() {
 	};
 	
 	inputClear.prototype.handleIcon = function() {
-		var self = this;
-		var value = this.widget.val();
-		
-		if (!value.length) {
-			this.widget.attr("data-ss-clear", "off");
-			return;
-		}
-		else {
-			this.widget.attr("data-ss-clear", "on");
-		}
+		this.widget.attr("data-ss-clear", (!this.widget.val().length) ? "off" : "on");
 	};
 	
 	return {
@@ -3305,7 +3317,6 @@ soysauce.togglers = (function() {
 			this.content = $(target).find("+ [data-ss-component='content']");
 
 			var collapse = (this.button.attr("data-ss-state") === "open" &&
-											this.button[0] === e.target &&
 											this.widget.find("[data-ss-component='button'][data-ss-state='open']").length === 1) ? true : false;
 			
 			if (collapse) {
