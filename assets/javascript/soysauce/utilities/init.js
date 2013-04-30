3f3091515fbc655146fde9f2b927f7b9a81cd87d
Edward Gaba <egaba88@gmail.com>
2013-04-30 07:47:18 -0400

    init - placing ret in correct place ensuring both deferred and non-deferred widgets return true

--- assets/javascript/soysauce/utilities/init.js
+++ assets/javascript/soysauce/utilities/init.js
@@ -53,6 +53,7 @@ soysauce.init = function(selector) {
 
 		if (widget !== undefined) {
 			soysauce.widgets.push(widget);
+			ret = true;
 			if ($this.attr("data-ss-defer") !== undefined) {
 				widget.defer = true;
 			}
@@ -61,14 +62,12 @@ soysauce.init = function(selector) {
 					widget.initialized = true;
 					$this.trigger("SSWidgetReady");
 				});
-				ret = true;
 			}
 		}
 		else {
 			$this.removeAttr("data-ss-id");
 			--soysauce.vars.idCount;
 		}
-		
 	});
 	
 	return ret;
