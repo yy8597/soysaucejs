f5ca928b061e1fd8770dde83934ed74d5181c2b6
Edward Gaba <egaba88@gmail.com>
2013-07-09 20:58:42 -0400

    lazyloader - added unload fallback for caching

--- assets/javascript/soysauce/widgets/lazyloader.js
+++ assets/javascript/soysauce/widgets/lazyloader.js
@@ -37,6 +37,7 @@ soysauce.lazyloader = (function() {
     });
 		
     if (this.cache) {
+      var unloaded = false;
       if (this.isCached) {
         this.widget.one("SSWidgetReady", function() {
           self.widget.trigger("SSLoadState");
@@ -45,7 +46,9 @@ soysauce.lazyloader = (function() {
       else {
         this.cacheInput.val(1);
       }
-      $(window).on("beforeunload", function() {
+      $(window).on("beforeunload unload", function(e) {
+        if (unloaded) return;
+        unloaded = true;
         self.widget.trigger("SSSaveState");
       });
     }
