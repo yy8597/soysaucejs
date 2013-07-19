b912950aae9c26bd51cd39554c3ace467059f0d3
Edward Gaba <egaba88@gmail.com>
2013-07-19 17:20:26 -0400

    soysauce.ajax() - added new parameter "forceAjax", added error checks/catches for full storage; always falls back to ajax'd data

--- assets/javascript/soysauce/utilities/ajax.js
+++ assets/javascript/soysauce/utilities/ajax.js
@@ -1,27 +1,29 @@
-soysauce.ajax = function(url) {
+soysauce.ajax = function(url, forceAjax) {
   var result = false;
   if (soysauce.browserInfo.supportsSessionStorage && sessionStorage[url]) {
     try {
       result = JSON.parse(sessionStorage[url]);
-      return result;
+      if (!forceAjax) return result;
     }
     catch(e) {}
   }
   $.ajax({
     url: url,
-    type: "GET",
     async: false
   }).success(function(data) {
     try {
       var resultString = JSON.stringify(data);
       result = JSON.parse(resultString);
-      if (soysauce.browserInfo.supportsSessionStorage) {
-        sessionStorage.setItem(url, resultString);
-      }
+      sessionStorage.setItem(url, resultString);
     }
     catch(e) {
-      console.warn("Soysauce: error fetching url '" + url + "'. Data returned needs to be JSON.");
-      result = false;
+      if (e.code === DOMException.QUOTA_EXCEEDED_ERR) {
+        console.warn("Soysauce: sessionStorage is full.");
+      }
+      else {
+        console.warn("Soysauce: error fetching url '" + url + "'. Data returned needs to be JSON.");
+        result = false;
+      }
     }
   }).fail(function(data) {
     console.warn("Soysauce: error fetching url '" + url + "'. Message: " + data.status + " " + data.statusText);
