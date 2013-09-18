a3d89e265f6bf666785e31b2426cf986793045ab
Edward Gaba <egaba88@gmail.com>
2013-09-18 13:44:28 -0400

    overlay - re-written as a soysauce object; now supports zoom carousels

--- assets/javascript/soysauce/utilities/overlay.js
+++ assets/javascript/soysauce/utilities/overlay.js
@@ -1,69 +1,136 @@
 soysauce.overlay = (function() {
-  var overlay, content, caption;
-  var init = true;
-  var isOn = false;
+  var TRANSITION_END = "transitionend webkitTransitionEnd oTransitionEnd MSTransitionEnd";
+  var $body = $("body");
+  var $viewport = $("meta[name='viewport']");
+  
+  function Overlay() {
+    this.overlay;
+    this.content;
+    this.close;
+    this.hiddenItems = null;
+    this.isOn = false;
+    
+    this.init();
+  };
+  
+  Overlay.prototype.init = function(selector) {
+    var div = document.createElement("div");
+    var self = this;
 
-  return {
-    init: function(selector) {
-      document.addEventListener("DOMContentLoaded", function() {
-        var div = document.createElement("div");
+    if ($("[data-ss-utility='overlay']").length) return false;
 
-        if (!init) return false;
+    div.setAttribute("data-ss-utility", "overlay");
+    div.setAttribute("data-ss-state", "inactive");
+    document.body.appendChild(div);
 
-        div.setAttribute("data-ss-utility", "overlay");
-        div.setAttribute("data-ss-state", "inactive");
-        document.body.appendChild(div);
+    this.overlay = $("[data-ss-utility='overlay']");
 
-        overlay = $("[data-ss-utility='overlay']");
+    this.overlay.append("<span data-ss-component='close'>close</span>");
+    this.close = this.overlay.find("[data-ss-component='close']");
+    
+    this.overlay.append("<div data-ss-component='content'></div>");
+    this.content = this.overlay.find("[data-ss-component='content']");
 
-        overlay.append("<span data-ss-component='close'>close</span>");
-        close = overlay.find("[data-ss-component='close']");
-        
-        overlay.append("<div data-ss-component='content'></div>");
-        content = overlay.find("[data-ss-component='content']");
+    this.close.on("click", function() {
+      self.off();
+    });
 
-        close.on("click", function() {
-          soysauce.overlay.off();
-        });
-
-        init = false;
-      });
-    },
-    on: function(css) {
-      if (isOn) return;
-      overlay.show();
-      window.setTimeout(function() {
-        if (css) {
-          try {
-            JSON.stringify(css);
-            overlay.css(css);
-          }
-          catch(e) {
-            console.warn("Soysauce: Could not attach css; need to pass JSON css object");
-          }
+    return true;
+  };
+  
+  Overlay.prototype.on = function(css) {
+    var self = this;
+    if (this.isOn) return;
+    this.overlay.show();
+    window.setTimeout(function() {
+      if (css) {
+        try {
+          JSON.stringify(css);
+          self.overlay.css(css);
         }
-        overlay.attr("data-ss-state","active");
-        isOn = true;
-      }, 0);
-    },
-    off: function() {
-      if (!isOn) return;
-      overlay.attr("data-ss-state","inactive").removeAttr("style");
-      overlay.hide();
-    },
-    toggle: function() {
-      if (isOn) {
-        soysauce.overlay.off();
+        catch(e) {
+          console.warn("Soysauce: Could not attach css; need to pass JSON css object");
+        }
       }
-      else {
-        soysauce.overlay.on();
-      }
-    },
-    getState: function() {
-      return isOn ? "on" : "off";
+      self.overlay.attr("data-ss-state","active");
+      self.isOn = true;
+    }, 0);
+  };
+  
+  Overlay.prototype.off = function() {
+    var viewportContent = "";
+    
+    if (!this.isOn) return;
+    
+    this.isOn = false;
+    this.overlay.attr("data-ss-state","inactive").removeAttr("style").hide();
+    this.content.empty();
+    
+    viewportContent = $viewport.attr("content");
+    $viewport.attr("content", viewportContent.replace(/(user-scalable=)([\w\.]+)/, "$1" + "no"));
+    viewportContent = viewportContent.replace(/(maximum-scale=)([\w\.]+)/, "$1" + "1.0");
+    $body.css({
+      "overflow": "",
+      "height": ""
+    });
+    
+    if (this.hiddenItems) {
+      this.hiddenItems.show();
+      this.hiddenItems = null;
     }
   };
-
+  
+  Overlay.prototype.toggle = function() {
+    if (this.isOn) {
+      this.off();
+    }
+    else {
+      this.on();
+    }
+  };
+  
+  Overlay.prototype.injectCarousel = function(carousel, css) {
+    var items = carousel.items.clone();
+    var $carousel;
+    var self = this;
+    
+    this.on(css);
+    
+    if (carousel.infinite) {
+      items = items.slice(1, carousel.numChildren - 1);
+    }
+    
+    items.removeAttr("data-ss-state").removeAttr("style");
+    this.content.wrapInner("<div data-ss-widget='carousel' data-ss-options='overlay'/>");
+    
+    $carousel = this.content.find("[data-ss-widget='carousel']");
+    $carousel.append(items);
+    
+    this.overlay.one(TRANSITION_END, function() {
+      $carousel.one("SSWidgetReady", function() {
+        var viewportContent = $viewport.attr("content");
+        $body.css({
+          "overflow": "hidden",
+          "height": "100%"
+        });
+        self.hiddenItems = $body.find("> *:not([data-ss-utility]):not(#ios7fix)");
+        self.hiddenItems.hide();
+        viewportContent = viewportContent.replace(/(user-scalable=)([\w\.]+)/, "$1" + "yes");
+        viewportContent = viewportContent.replace(/(maximum-scale=)([\w\.]+)/, "$1" + "4.0");
+        $viewport.attr("content", viewportContent);
+      });
+      soysauce.init($carousel[0]);
+    });
+  };
+  
+  Overlay.prototype.hideAssets = function() {
+    this.close.css("opacity", "0");
+  };
+  
+  Overlay.prototype.showAssets = function() {
+    this.close.css("opacity", "1");
+  };
+  
+  return new Overlay();
+  
 })();
-
-soysauce.overlay.init();
