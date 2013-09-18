0bc855f4eaf4b1857aa4f4c32736540fed4a961a
Edward Gaba <egaba88@gmail.com>
2013-09-17 21:20:56 -0400

    overlay - changed 'done' button to 'close'; removed caption section; added content section

--- assets/javascript/soysauce/utilities/overlay.js
+++ assets/javascript/soysauce/utilities/overlay.js
@@ -1,5 +1,5 @@
 soysauce.overlay = (function() {
-  var overlay, done, caption;
+  var overlay, content, caption;
   var init = true;
   var isOn = false;
 
@@ -16,34 +16,40 @@ soysauce.overlay = (function() {
 
         overlay = $("[data-ss-utility='overlay']");
 
-        overlay.append("<span class='done'></span>");
-        done = overlay.find(".done");
+        overlay.append("<span data-ss-component='close'>close</span>");
+        close = overlay.find("[data-ss-component='close']");
+        
+        overlay.append("<div data-ss-component='content'></div>");
+        content = overlay.find("[data-ss-component='content']");
 
-        overlay.append("<div class='caption'></span>");
-        caption = overlay.find(".caption");
-
-        done.on("click", function() {
+        close.on("click", function() {
           soysauce.overlay.off();
         });
 
         init = false;
       });
     },
-    on: function() {
+    on: function(css) {
       if (isOn) return;
       overlay.show();
       window.setTimeout(function() {
+        if (css) {
+          try {
+            JSON.stringify(css);
+            overlay.css(css);
+          }
+          catch(e) {
+            console.warn("Soysauce: Could not attach css; need to pass JSON css object");
+          }
+        }
         overlay.attr("data-ss-state","active");
         isOn = true;
       }, 0);
     },
     off: function() {
       if (!isOn) return;
-      overlay.attr("data-ss-state","inactive");
-      window.setTimeout(function() {
-        overlay.hide();
-        isOn = false;
-      }, 400);
+      overlay.attr("data-ss-state","inactive").removeAttr("style");
+      overlay.hide();
     },
     toggle: function() {
       if (isOn) {
@@ -53,21 +59,8 @@ soysauce.overlay = (function() {
         soysauce.overlay.on();
       }
     },
-    state: function() {
-      if (isOn) {
-        return "on";
-      }
-      else {
-        return "off";
-      }
-    },
-    caption: function(text) {
-      if (!text) {
-        caption.html("");
-      }
-      else {
-        caption.html(text);
-      }
+    getState: function() {
+      return isOn ? "on" : "off";
     }
   };
 
