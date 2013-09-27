57383613e9bf7b27a6b54a76e4654cb570e21d44
Edward Gaba <egaba88@gmail.com>
2013-09-27 16:22:52 -0400

    base - orientationchange event changed to resize

--- assets/javascript/soysauce/base.js
+++ assets/javascript/soysauce/base.js
@@ -279,25 +279,23 @@ soysauce = {
 }
 
 // Widget Resize Handler
-$(window).on("orientationchange", function(e) {
-  if (e.type === "orientationchange") {
-    if (soysauce.vars.lastResizeID) clearTimeout(soysauce.vars.lastResizeID);
-    soysauce.vars.lastResizeID = window.setTimeout(function() {
-      soysauce.vars.lastResizeTime = e.timeStamp;
-      soysauce.widgets.forEach(function(widget) {
-        if (!widget.handleResize) return;
-        widget.handleResize();
-        if (/carousel/i.test(widget.type)) {
-          if (widget.itemWidth) {
-            $(widget.widget).trigger("SSWidgetResized");
-          }
-        }
-        else {
+$(window).on("resize", function(e) {
+  if (soysauce.vars.lastResizeID) clearTimeout(soysauce.vars.lastResizeID);
+  soysauce.vars.lastResizeID = window.setTimeout(function() {
+    soysauce.vars.lastResizeTime = e.timeStamp;
+    soysauce.widgets.forEach(function(widget) {
+      if (!widget.handleResize) return;
+      widget.handleResize();
+      if (/carousel/i.test(widget.type)) {
+        if (widget.itemWidth) {
           $(widget.widget).trigger("SSWidgetResized");
         }
-      });
-    }, 30);
-  }
+      }
+      else {
+        $(widget.widget).trigger("SSWidgetResized");
+      }
+    });
+  }, 30);
 });
 
 // Widget Initialization
