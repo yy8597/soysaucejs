6e657768f7c0e32da9c4bd3cc19ce661093ab4c8
Edward Gaba <egaba88@gmail.com>
2013-10-12 10:02:56 -0400

    clean dir

--- assets/javascript/site/widgets.js
+++
@@ -1,9 +0,0 @@
-$(document).ready(function() {
-  $(".widgets").each(function() {
-    var navItems = $(".api aside li");
-    var apiCarousel = soysauce.fetch(".apiCarousel");
-    navItems.click(function(e, i) {
-      apiCarousel.jumpTo(navItems.index(this));
-    });
-  });
-});
